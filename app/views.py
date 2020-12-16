2ac36bc5a15af5c4077f3b75072e5596f86d4229
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2020-12-16 20:47:02 +0900

    新規投稿viewを記述

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from.models import Post
-
+from.forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -16,4 +17,12 @@ class PostDetailView(View):
         post_data = Post.objects.get(id=self.kwargs['pk'])
         return render(request, 'app/post_detail.html', {
             'post_data': post_data
+        })
+
+class CreatePostView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        form = PostForm(request.POST or None)
+
+        return render(request, 'app/post_form.html', {
+            'form': form
         })
